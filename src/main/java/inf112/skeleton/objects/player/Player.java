56d32a1bcbeffe5a8b350a47e7cea245ec16a6b1
Runar Sunde <rusun3656@uib.no>
2024-03-19 16:10:10 +0100

    Contact and movement

--- src/main/java/inf112/skeleton/objects/player/Player.java
+++ src/main/java/inf112/skeleton/objects/player/Player.java
@@ -1,8 +1,9 @@
 package inf112.skeleton.objects.player;
 
 import static inf112.skeleton.helper.Constants.PPM;
+import static inf112.skeleton.helper.MyContactListener.groundAngle;
 import static inf112.skeleton.helper.MyContactListener.isOnContact;
-
+import static inf112.skeleton.helper.MyContactListener.isOnSlope;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -13,18 +14,17 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Fixture;
 
-
-
-public class Player extends GameEntity{
+public class Player extends GameEntity {
     private int jumpCounter;
     private int framesGrounded;
     private Texture playerTexture;
+    private float slideForceMagnitude = 1.0f;
 
     public Player(float width, float height, Body body) {
         super(width, height, body);
         this.speed = 4f;
         this.jumpCounter = 0;
-    
+
         playerTexture = new Texture("images/button.jpg");
         for (Fixture fixture : body.getFixtureList()) {
             fixture.setUserData("player");
@@ -35,54 +35,72 @@ public class Player extends GameEntity{
     public void update() {
         x = body.getPosition().x * PPM;
         y = body.getPosition().y * PPM;
-        
+
         checkUserInput();
-
-
     }
 
-    public float getx(){
+    public float getx() {
         return x;
     }
-    public float gety(){
+
+    public float gety() {
         return y;
     }
-    public float getWidth(){
+
+    public float getWidth() {
         return width;
     }
-    public float getHeight(){
+
+    public float getHeight() {
         return height;
     }
+
     @Override
     public void render(SpriteBatch batch) {
-/*         batch.begin();
-        batch.draw(playerTexture, this.getx() - this.getWidth()/2, this.gety() - this.getHeight()/2, this.getWidth(), this .getHeight());
-        batch.end(); */
+        /*
+         * batch.begin();
+         * batch.draw(playerTexture, this.getx() - this.getWidth()/2, this.gety() -
+         * this.getHeight()/2, this.getWidth(), this .getHeight());
+         * batch.end();
+         */
     }
-    
-    private void checkUserInput(){
 
-        if(!isOnContact && jumpCounter == 0){
+    private void checkUserInput() {
+
+        if (isOnSlope && jumpCounter == 0) {
             jumpCounter = 1;
         }
 
-        if((body.getLinearVelocity().y == 0) && framesGrounded != 5){
+        if ((body.getLinearVelocity().y == 0) && framesGrounded != 5) {
             framesGrounded++;
-            if(framesGrounded == 5){
+            if (framesGrounded == 5) {
                 jumpCounter = 0;
             }
         }
-        if(Gdx.input.isKeyJustPressed(Input.Keys.W) && jumpCounter < 2){
+        
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W) && jumpCounter < 2) {
             float force = body.getMass() * 10;
             body.setLinearVelocity(body.getLinearVelocity().x, 0);
             body.applyLinearImpulse(new Vector2(0, force), body.getPosition(), true);
             framesGrounded = 0;
             jumpCounter++;
         }
-        
-        body.setLinearVelocity(velX * speed, body.getLinearVelocity().y < 18 ? body.getLinearVelocity().y : 18);
+        move();
     }
 
+    public void move() {
+        // Check if the player is on a slope
+        // Check if the slope angle is significant (not flat)
+        if (isOnSlope && !isOnContact) {                                            // && Math.abs(groundAngle) < 0.3f does not work as intended
+            // Calculate the direction of the slide force based on the slope angle
+            Vector2 slideDirection = new Vector2((float) Math.sin(groundAngle), (float) Math.cos(groundAngle));
+            // Apply the slide force to the player's body
+            body.applyForceToCenter(slideDirection.scl(slideForceMagnitude), true);
+        }
+        else {
+            body.setLinearVelocity(velX * speed, body.getLinearVelocity().y < 18 ? body.getLinearVelocity().y : 18);
+        }
     
-    
+    }
+
 }
