fbcc123c34f195c96bca76cbff2a265d36541625
Marius <marius_f_nyland@hotmail.com>
2024-05-05 22:37:27 +0200

    test

--- src/main/java/inf112/skeleton/states/PlayState.java
+++ src/main/java/inf112/skeleton/states/PlayState.java
@@ -142,7 +142,7 @@ public class PlayState extends State{
         // enemy.render(batch);
         for (GameEntity enemy : enemies) {
                enemy.render(batch);
-           }
+        }
     
         if(debug){
             box2dDebugRenderer.render(world, cam.combined.scl(PPM));
